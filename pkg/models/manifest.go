b7ac37a39934fc5aec38ece4a1f9944e6deadd63
Sergey <xxsmotur@gmail.com>
2020-04-27 20:50:40 +0500

    Extend Manifest format

--- pkg/models/manifest.go
+++ pkg/models/manifest.go
@@ -2,8 +2,11 @@ package models
 
 // Manifest defines spec for image manifest
 type Manifest struct {
-	Name   string  `json:"name"`
-	Layers []Layer `json:"fsLayers"`
+	Name         string   `json:"name"`
+	Tag          string   `json:"tag"`
+	Architecture string   `json:"architecture"`
+	Layers       []Layer  `json:"fsLayers"`
+	History      []string `json:"history"`
 }
 
 // Layer defines representation of layer
