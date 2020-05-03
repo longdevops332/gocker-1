be1c4d763700465902a067c9b153fa502e4f8730
Sergey <xxsmotur@gmail.com>
2020-05-03 17:27:18 +0500

    Extending definition of manifest structs

--- pkg/models/manifest.go
+++ pkg/models/manifest.go
@@ -1,12 +1,28 @@
 package models
 
+import (
+	"time"
+)
+
 // Manifest defines spec for image manifest
 type Manifest struct {
-	Name         string   `json:"name"`
-	Tag          string   `json:"tag"`
-	Architecture string   `json:"architecture"`
-	Layers       []Layer  `json:"fsLayers"`
-	History      []string `json:"history"`
+	Name         string    `json:"name"`
+	Tag          string    `json:"tag"`
+	Architecture string    `json:"architecture"`
+	Layers       []Layer   `json:"fsLayers"`
+	History      []History `json:"history"`
+}
+
+// History provides definition of container exec
+type History struct {
+	V1Compatibility string `json:"v1Compatibility"`
+}
+
+// V1Compatibility ..
+type V1Compatibility struct {
+	ID      string    `json:"id"`
+	Cmd     string    `json:"Cmd"`
+	Created time.Time `json:"created"`
 }
 
 // Layer defines representation of layer
